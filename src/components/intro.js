aef02d0f6d44ef9b989bfbdceea69feaec1624d2
Sifeng Chen <sifengchen2002@gmail.com>
2023-06-03 04:10:58 -0400

    eslint

--- src/components/intro.js
+++ src/components/intro.js
@@ -56,7 +56,9 @@ const Intro = () => {
           id="pContainer"
           className="mt-[20px] flex flex-col justify-start w-[100%]"
         >
-        <p className="text-white font-mono text-md"> Hi, I'm Sifeng Chen</p>
+          <p className="text-white font-mono text-md">
+            Hi, I{"'"}m Sifeng Chen
+          </p>
           <p className="mt-[12px] text-white font-mono text-sm">
             I{" "}
             <Typical

82e28d2e7d1009813d55feb4fce7af672cdea20e
Sifeng Chen <sifengchen2002@gmail.com>
2023-06-13 03:36:53 -0400

    animated lines

--- src/components/intro.js
+++ src/components/intro.js
@@ -10,7 +10,7 @@ const InitialLogoStates = {
   middle: {
     opacity: 1,
     transition: {
-      duration: 3.5,
+      duration: 4,
     },
   },
   exit: {
@@ -35,6 +35,21 @@ export const InitialLogo = () => {
   );
 };
 
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i) => {
+    const delay = 1 + i * 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
+
 const Intro = () => {
   const [flashy, setFlashy] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
@@ -45,11 +60,93 @@ const Intro = () => {
     <>
       <div
         id="IntroContainer"
-        className="flex flex-col items-center border- w-[350px]"
+        className="flex flex-col items-center border- w-[350px] relative"
       >
         <motion.div onMouseEnter={handleHover} className="">
           {/* <button className="text-red-500">Toggle Image</button> */}
           <InitialLogo />
+          <motion.svg
+            width="100%"
+            height="200"
+            viewBox="35 140 500 140"
+            initial="hidden"
+            animate="visible"
+            className="absolute top-0 left-[-5px] stroke-[4px] stroke-rounded"
+          >
+            <motion.line
+              x1="102"
+              y1="280"
+              x2="102"
+              y2="100"
+              stroke="#0099ff"
+              variants={draw}
+              custom={1}
+              className="text-frame stroke-current"
+            />
+
+            <motion.line
+              x1="480"
+              y1="280"
+              x2="480"
+              y2="100"
+              stroke="#0099ff"
+              variants={draw}
+              custom={3}
+              className="text-frame stroke-current"
+            />
+            <motion.line
+              x1="483"
+              y1="280"
+              x2="483"
+              y2="100"
+              stroke="#0099ff"
+              variants={draw}
+              custom={3}
+              className="text-frame stroke-current"
+            />
+            <motion.line
+              x1="486"
+              y1="280"
+              x2="486"
+              y2="100"
+              stroke="#0099ff"
+              variants={draw}
+              custom={3}
+              className="text-frame stroke-current"
+            />
+            <motion.line
+              x1="489"
+              y1="280"
+              x2="489"
+              y2="100"
+              stroke="#0099ff"
+              variants={draw}
+              custom={3}
+              className="text-frame stroke-current"
+            />
+
+            <motion.line
+              x1="480"
+              y1="102"
+              x2="150"
+              y2="102"
+              stroke="#0099ff"
+              variants={draw}
+              custom={2}
+              className="text-frame stroke-current"
+            />
+
+            <motion.line
+              x2="440"
+              y2="278"
+              x1="100"
+              y1="278"
+              stroke="#0099ff"
+              variants={draw}
+              custom={4.5}
+              className="text-frame stroke-current"
+            />
+          </motion.svg>
         </motion.div>
 
         <div

9b27cdcb7758b5108d17bebaf4e267a6d5628a89
Sifeng Chen <sifengchen@Sifengs-MacBook-Air.local>
2025-10-27 19:54:08 -0400

    fix gh action

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
+ reactStrictMode: true,
+ images: {
+  unoptimized: true,
+ },
+ output: "export",
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
